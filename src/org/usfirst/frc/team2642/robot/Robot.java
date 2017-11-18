eace806bc6030c9cd90c7baf9a75d9172cb38b5f
Christian Colglazier <aquamorphcomputing@gmail.com>
2017-11-18 08:10:40 -0500

    Added camera

--- src/org/usfirst/frc/team2642/robot/Robot.java
+++ src/org/usfirst/frc/team2642/robot/Robot.java
@@ -1,6 +1,8 @@
 
 package org.usfirst.frc.team2642.robot;
 
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -9,11 +11,33 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends IterativeRobot {
 
 	public static OI oi;
+	public static UsbCamera camera;
+
+	//Changes camera settings for vision tracking
+	public static void enableVisionTracking(boolean enabled) {
+		if (enabled) { //Vision Mode
+			camera.setBrightness(0);
+			camera.setExposureManual(0);
+			RobotMap.visionEnabled = true;
+
+		} else { //Driving Mode
+			camera.setBrightness(30);
+			camera.setExposureManual(35);
+			RobotMap.visionEnabled = false;
+		}
+	}
 
 	@Override
 	public void robotInit() {
 		oi = new OI();
 		SmartDashboard.putString("Robot Code Version", RobotMap.robotVersion);
+
+		// Set up camera
+		camera = CameraServer.getInstance().startAutomaticCapture("Camera", RobotMap.CAMERA_USB);
+		camera.setResolution(RobotMap.IMG_WIDTH, RobotMap.IMG_HEIGHT);
+		camera.setFPS(RobotMap.CAMERA_FPS);
+
+		enableVisionTracking(RobotMap.visionEnabled);
 	}
 
 	@Override
--- src/org/usfirst/frc/team2642/robot/RobotMap.java
+++ src/org/usfirst/frc/team2642/robot/RobotMap.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team2642.robot;
 
 public class RobotMap {
-	public static final String robotVersion = "0.1.0";
+	static final String robotVersion = "0.1.0";
+
+	// Camera resolution
+	static final int CAMERA_USB = 0;
+	static final int IMG_WIDTH = 320;
+	static final int IMG_HEIGHT = 240;
+	static final int CAMERA_FPS = 10;
+	public static boolean visionEnabled = false;
 }
